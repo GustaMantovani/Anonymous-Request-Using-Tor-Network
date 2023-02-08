eb9676407092a19435182755f642aef3c9bba923
GustaMantovani <mantovanig000@gmail.com>
2023-02-07 22:01:54 -0300

    rework

--- tor.py
+++ tor.py
@@ -6,27 +6,33 @@ session.proxies = {}
 session.proxies['http'] = 'socks5h://localhost:9050'
 session.proxies['https'] = 'socks5h://localhost:9050'
 
-while True :
-    dest = str(input('target: '))
-    try :
-        while True :
+print ('____________________________YOU__________________________________')
+source = requests.get('http://httpbin.org/ip')
+fake = session.get('http://httpbin.org/ip')
+print( 'your ip: ', source.text, 'tor ip: ', fake.text)
+print ('_________________________________________________________________')
+print('')
 
-            print ('____________________________YOU__________________________________')
-            source = requests.get('http://httpbin.org/ip')
-            fake = session.get('http://httpbin.org/ip')
-            print( 'your ip: ', source.text, 'tor ip: ', fake.text)
+current_status = 404
+reply = ''
 
-            reply = session.get(dest)
-            print ('____________________________REPLY____________________________________')
-            if (reply == '') :
-                print('no response, status code: ', reply)
-            else:
-                print(reply.text)
+while True:
 
-    except Exception as e:
-        print ('____________________________ERROR____________________________________')
-        print('destination host unreachable or unsupported')
+    if current_status == 404:
+        dest = str(input('target: '))
+
+    previous_reply = reply
+    reply = session.get(dest)
+
+    previous_status = current_status
+    current_status=reply.status_code
+
+    if current_status != previous_status:
+        print('STATUS CODE CHANGED: ', current_status)
         print('')
-        print('')
-   
+        print('REPLY: ')
+
+        if reply != previous_reply:
+            print(reply.text)
+       
 

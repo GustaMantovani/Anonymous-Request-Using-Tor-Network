da9e2880c5f128569018ff21dd22b6d988a52f62
GustaMantovani <mantovanig000@gmail.com>
2023-02-10 14:17:36 -0300

    Update!

--- tor.py
+++ tor.py
@@ -12,27 +12,46 @@ print( 'your ip: ', source.text, 'tor ip: ', fake.text)
 print ('_________________________________________________________________')
 print('')
 
-current_status = 404
+dest = str(input('target: '))
 reply = ''
-
+e = ''
+current_status = ''
+   
 while True:
 
-    if current_status == 404:
-        print('not found')
+    if e != '':
+        print("error:", e)
         dest = str(input('target: '))
 
-    previous_reply = reply
-    reply = session.get(dest)
+    elif current_status == 404:
+        print('NOT FOUND')
+        dest = str(input('target: '))
 
+    try:
+        reply = session.get(dest)
+
+    except :
+        e = Exception
+
+    previous_reply = reply
     previous_status = current_status
     current_status=reply.status_code
 
-    if current_status != previous_status:
-        print('STATUS CODE CHANGED: ', current_status)
-        print('')
+    if current_status != 404:
+        if previous_reply == '' or previous_status == '':
+            print('')
+            print('STATUS CODE: ', reply.status_code)
+            print('')
+            print('REPLY: ', reply.text)
 
-        if reply != previous_reply:
-            print('REPLY: ')
-            print(reply.text)
+        else:
+            if current_status != previous_status:
+                print('STATUS CODE CHANGED: ', current_status)
+                print('')
+                print('NEW REPLY: ')
+                print(reply.text)
+
+            if reply.text != previous_reply.text:
+                print('NEW REPLY: ')
+                print(reply.text)
        
-

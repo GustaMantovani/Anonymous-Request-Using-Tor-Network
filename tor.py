e4b7bf1302c136ab245ed40d1e956842a613886d
GustaMantovani <mantovanig000@gmail.com>
2023-08-07 15:57:26 -0300

    rebuild
    Reorganizando funções e o método de realizar requests em loop

--- tor.py
+++ tor.py
@@ -1,57 +1,42 @@
 import requests
 
-session = requests.session()
-session.proxies = {}
-session.proxies['http'] = 'socks5h://localhost:9050'
-session.proxies['https'] = 'socks5h://localhost:9050'
+def setSession():
+    session = requests.session()
+    session.proxies = {}
+    session.proxies['http'] = 'socks5h://localhost:9050'
+    session.proxies['https'] = 'socks5h://localhost:9050'
 
-print ('____________________________YOU__________________________________')
-source = requests.get('http://httpbin.org/ip')
-fake = session.get('http://httpbin.org/ip')
-print( 'your ip: ', source.text, 'tor ip: ', fake.text)
-print ('_________________________________________________________________')
-print('')
+    return session
 
-dest = str(input('target: '))
-reply = ''
-e = ''
-current_status = ''
-   
-while True:
+def panel(session):
+    print ('____________________________YOU__________________________________')
+    source = requests.get('http://httpbin.org/ip')
+    fake = session.get('http://httpbin.org/ip')
+    print( 'your ip: ', source.text, 'tor ip: ', fake.text)
+    print ('_________________________________________________________________')
+    print('')
 
-    if e != '':
-        print("error:", e)
-        dest = str(input('target: '))
-
-    elif current_status == 404:
-        print('NOT FOUND')
-        dest = str(input('target: '))
-
-    try:
-        reply = session.get(dest)
-
-    except :
-        e = Exception
-
-    previous_reply = reply
-    previous_status = current_status
-    current_status=reply.status_code
-
-    if current_status != 404:
-        if previous_reply == '' or previous_status == '':
-            print('')
-            print('STATUS CODE: ', reply.status_code)
-            print('')
-            print('REPLY: ', reply.text)
-
+def requestAndDisplayResponses (session):
+    target = input("target: ")
+    while True:
+        try: 
+            rep = session.get(target)
+            print(rep.status_code)
+            while str(rep.status_code)=='404':
+                target = input("Not Found\ntarget: ")
+                rep = session.get(target)
+        except Exception as e:
+            print("Error: ", e)
+            break
         else:
-            if current_status != previous_status:
-                print('STATUS CODE CHANGED: ', current_status)
-                print('')
-                print('NEW REPLY: ')
-                print(reply.text)
+            print("STATUS CODE: ", rep.status_code)
+            print("REPLY: ", rep.text)
 
-            if reply.text != previous_reply.text:
-                print('NEW REPLY: ')
-                print(reply.text)
+def main():
+    session = setSession()
+    panel(session)
+    requestAndDisplayResponses (session)
+
+main()
+
        
